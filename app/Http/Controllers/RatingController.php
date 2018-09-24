29d2e0b2cfee3b81520d02cb7ceb19f09f47822c
Mantas Damijonaitis <m.damijonaitis@gmail.com>
2018-09-24 17:47:29 +0300

    finished first laboratory

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+use App\Rating;
+use App\Http\Resources\RatingResource;
+
+class RatingController extends Controller
+{
+
+	public function __construct()
+	{
+		$this->middleware('auth:api')->except(['index', 'show']);
+	}
+
+    //
+	public function store(Request $request, Book $book)
+	{
+		$rating = Rating::firstOrCreate(
+			[
+				'user_id' => $request->user()->id,
+				'book_id' => $book->id,
+			],
+			['rating' => $request->rating]
+		);
+
+		return new RatingResource($rating);
+	}
+}
